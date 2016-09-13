19374c0eac1941495ccc821c6486af6af9c1785a
Bill Columbia <bcolumbia@ufl.edu>
2016-09-13 08:57:59 -0400

    refactor

--- vue/src/vueTextMask.js
+++ vue/src/vueTextMask.js
@@ -6,38 +6,36 @@ import createTextMaskInputElement from '../../core/src/createTextMaskInputElemen
  *
  * This directive binds input masking via Text Mask. Once the element is
  * destroyed it will be unbinded. Pass an options object, same as you would
- * normally for this plugin, as a parameter on the element being bound.
+ * normally, as a parameter on the element being bound.
  */
 
-let textMaskInputElement
-
-// Use core function to init the mask
-const setupTextMask = options => {
-  textMaskInputElement = createTextMaskInputElement(options)
-}
+/**
+ * Initialize variable to store directive's `this` so that we can use its
+ * context in inputHandler called via event listener
+ */
+let instance
 
 // Register the input handler to watch for updates
 const inputHandler = ({target: {value}}) => {
-  return textMaskInputElement.update(value)
+  return instance.textMaskInputElement.update(value)
 }
 
 export default {
+  textMaskInputElement: 0,
   params: ['maskOptions'],
   /**
-   * via Vue API
-   *
    * Use the element (should be a text input) calling the directive as an
    * option for the text mask. Expect options as params, but if none are sent
    * just use the element as the only option. Send all options together in an
-   * object.
+   * object to createTextMaskInputElement (from Core). Add an event listener to
+   * watch and update with changes.
    */
   bind() {
-    // Get options as params
-    let options = this.params.maskOptions || {}
-    // Set the input as an option for text-mask
-    options.inputElement = this.el
-    setupTextMask(options)
-    this.el.addEventListener('input', inputHandler)
+    instance = this
+    let options = instance.params.maskOptions || {}
+    options.inputElement = instance.el
+    instance.textMaskInputElement = createTextMaskInputElement(options)
+    instance.el.addEventListener('input', inputHandler)
   },
   /**
    * via Vue API
@@ -46,6 +44,6 @@ export default {
    * protect the application / tidy from memory leaks.
    */
   unbind() {
-    this.el.removeEventListener('input', inputHandler)
+    instance.el.removeEventListener('input', inputHandler)
   }
 }
